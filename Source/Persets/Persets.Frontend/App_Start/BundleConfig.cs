1ef1887383612ccfb333618a85b1d99c6ca4c105
logonrm <logonrm@L2201MICRO24>
2017-03-11 15:40:08 -0300

    Ajustado Bundle e removido arquivos.

---
+++ Source/Persets/Persets.Frontend/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Persets.Frontend
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/content/css")
+                .Include("~/scripts/bootstrap/dist/css/bootstrap.css",
+                         "~/scripts/font-awesome/css/font-awesome.css",
+                         "~/scripts/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.css",
+                         "~/content/css/custom.min.css"));
+
+            bundles.Add(new ScriptBundle("~/scripts/js")
+                .Include("~/scripts/jquery/dist/jquery.js",
+                         "~/scripts/bootstrap/dist/js/bootstrap.js",
+                         "~/scripts/bootstrap-progressbar/bootstrap-progressbar.js",
+                         "~/scripts/iCheck/icheck.min.js",
+                         "~/content/js/custom.min.js"));
+        }
+    }
+}
