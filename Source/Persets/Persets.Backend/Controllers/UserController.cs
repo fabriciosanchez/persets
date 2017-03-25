7d1baeac4e43b744400075ad314c4acb0dfb1179
Raphael Divetta Bronzo <divetta@gmail.com>
2017-03-25 11:24:24 -0300

    Ajustando a gravação dos usuários!

--- Source/Persets/Persets.Backend/Controllers/UserController.cs
+++ Source/Persets/Persets.Backend/Controllers/UserController.cs
@@ -6,6 +6,7 @@ using Persets.Backend.Interfaces;
 using System.Web.Http;
 using Persets.Backend.Models;
 using Persets.Backend.Services;
+using Persets.Backend.Models.ViewModels;
 
 namespace Persets.Backend.Controllers
 {
@@ -19,48 +20,78 @@ namespace Persets.Backend.Controllers
         {
             _membershipService = membershipService;
         }
+        
+        [Route("api/users")]
+        [HttpPost]
+        public HttpResponseMessage Register(HttpRequestMessage request, RegistrationViewModel user)
+        {
+            HttpResponseMessage response;
 
-
-  
-       
-                [Route("api/users")]
-                [HttpPost]
-                public HttpResponseMessage Register(HttpRequestMessage request, RegistrationViewModel user)
+            try
+            {
+                if (!ModelState.IsValid)
                 {
-                    HttpResponseMessage response;
-
-                    try
-                    {
-                        if (!ModelState.IsValid)
-                        {
-                            response = request.CreateResponse(HttpStatusCode.BadRequest, new { success = false });
-                        }
-                        else
-                        {
-                            Users _user = _membershipService.CreateUser(user.CompleteName, user.UserName, user.Email, user.Password, user.BirthdayDate);
-
-                            if (_user != null)
-                            {
-                                response = request.CreateResponse(HttpStatusCode.OK, new { success = true });
-                            }
-                            else
-                            {
-                                response = request.CreateResponse(HttpStatusCode.OK, new { success = false });
-                            }
-                        }
-
-                        return response;
-                    }
-                    catch (DbUpdateException ex)
-                    {
-                        response = request.CreateResponse(HttpStatusCode.BadRequest, ex.InnerException.Message);
-                    }
-                    catch (Exception ex)
-                    {
-                        response = request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
-                    }
-
-                    return response;
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, new { success = false });
                 }
+                else
+                {
+                    Users _user = _membershipService.CreateUser(user.CompleteName, user.UserName, user.Email, user.Password, user.BirthdayDate);
+
+                    if (_user != null)
+                    {
+                        response = request.CreateResponse(HttpStatusCode.OK, new { success = true });
+                    }
+                    else
+                    {
+                        response = request.CreateResponse(HttpStatusCode.OK, new { success = false });
+                    }
+                }
+
+                return response;
+            }
+            catch (DbUpdateException ex)
+            {
+                response = request.CreateResponse(HttpStatusCode.BadRequest, ex.InnerException.Message);
+            }
+            catch (Exception ex)
+            {
+                response = request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+            return response;
+        }
+
+        [Route("api/users/forgot")]
+        [HttpPost]
+        public HttpResponseMessage ForgotPassword(HttpRequestMessage request, ForgotPasswordViewModel user)
+        {
+            HttpResponseMessage response;
+
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, new { success = false });
+                }
+                else
+                {
+                    string password = _membershipService.ForgotPassword(user.Email);
+
+                    response = request.CreateResponse(HttpStatusCode.OK, password);
+                }
+
+                return response;
+            }
+            catch (DbUpdateException ex)
+            {
+                response = request.CreateResponse(HttpStatusCode.BadRequest, ex.InnerException.Message);
+            }
+            catch (Exception ex)
+            {
+                response = request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+            return response;
+        }
     }
 }
