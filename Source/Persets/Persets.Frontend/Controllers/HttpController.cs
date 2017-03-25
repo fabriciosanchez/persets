02ba6d1aa025fc20cee29d1d477ffe01f1e9e9e5
Henrique <riquemorato@gmail.com>
2017-03-25 09:00:11 -0300

    Http static

---
+++ Source/Persets/Persets.Frontend/Controllers/HttpController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace Persets.Frontend.Controllers
+{
+    public class HttpController
+    {
+        public static HttpClient client = new HttpClient();
+
+        public void Initialize ()
+        {
+            client.BaseAddress = new Uri("http://localhost:25241/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
