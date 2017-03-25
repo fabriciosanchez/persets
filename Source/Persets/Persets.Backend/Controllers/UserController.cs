5bb8cd79d59a35edc6c348b8d79edc890ebcd84b
Diogo Koshiba <diogo.koshiba@gmail.com>
2017-03-25 10:52:44 -0300

    add log create user

--- Source/Persets/Persets.Backend/Controllers/UserController.cs
+++ Source/Persets/Persets.Backend/Controllers/UserController.cs
@@ -6,6 +6,7 @@ using Persets.Backend.Interfaces;
 using System.Web.Http;
 using Persets.Backend.Models;
 using Persets.Backend.Services;
+using Persets.Backend.Data;
 
 namespace Persets.Backend.Controllers
 {
@@ -14,53 +15,53 @@ namespace Persets.Backend.Controllers
     public class UserController : ApiController
     {
         private readonly IMembershipService _membershipService;
-     
+
         public UserController(IMembershipService membershipService)
         {
             _membershipService = membershipService;
         }
 
+        [Route("api/users")]
+        [HttpPost]
+        public HttpResponseMessage Register(HttpRequestMessage request, RegistrationViewModel user)
+        {
+            HttpResponseMessage response;
+            string EntityGuid = "~";
 
-  
-       
-                [Route("api/users")]
-                [HttpPost]
-                public HttpResponseMessage Register(HttpRequestMessage request, RegistrationViewModel user)
+            try
+            {
+                if (!ModelState.IsValid)
                 {
-                    HttpResponseMessage response;
-
-                    try
-                    {
-                        if (!ModelState.IsValid)
-                        {
-                            response = request.CreateResponse(HttpStatusCode.BadRequest, new { success = false });
-                        }
-                        else
-                        {
-                            Users _user = _membershipService.CreateUser(user.CompleteName, user.UserName, user.Email, user.Password, user.BirthdayDate);
-
-                            if (_user != null)
-                            {
-                                response = request.CreateResponse(HttpStatusCode.OK, new { success = true });
-                            }
-                            else
-                            {
-                                response = request.CreateResponse(HttpStatusCode.OK, new { success = false });
-                            }
-                        }
-
-                        return response;
-                    }
-                    catch (DbUpdateException ex)
-                    {
-                        response = request.CreateResponse(HttpStatusCode.BadRequest, ex.InnerException.Message);
-                    }
-                    catch (Exception ex)
-                    {
-                        response = request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
-                    }
-
-                    return response;
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, new { success = false });
                 }
+                else
+                {
+                    Users _user = _membershipService.CreateUser(user.CompleteName, user.UserName, user.Email, user.Password, user.BirthdayDate);
+
+                    if (_user != null)
+                    {
+                        EntityGuid = _user.GUID;
+                        response = request.CreateResponse(HttpStatusCode.OK, new { success = true });
+                    }
+                    else
+                    {
+                        response = request.CreateResponse(HttpStatusCode.OK, new { success = false });
+                    }
+                }
+            }
+            catch (DbUpdateException ex)
+            {
+                response = request.CreateResponse(HttpStatusCode.BadRequest, ex.InnerException.Message);
+            }
+            catch (Exception ex)
+            {
+                response = request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+            if (response != null)
+                LogsDB.AddLog(LogsDB.eOperation.Post, LogsDB.eEntity.User, EntityGuid, response.IsSuccessStatusCode);
+
+            return response;
+        }
     }
 }
