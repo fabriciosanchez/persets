4e3bbe4b0ac9a9519ce340ff82a2d3078452aeb4
rogerio santos <rogerio.azzevedo@outlook.com>
2017-03-25 10:55:55 -0300

    adicionando controller do content

---
+++ Source/Persets/Persets.Backend/Controllers/ContentsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Persets.Backend.Models;
+using Persets.Backend.Models.ViewModels;
+
+namespace Persets.Backend.Controllers
+{
+    public class ContentsController : ApiController
+    {
+        private PersetsDBEntities db = new PersetsDBEntities();
+
+        // GET: api/Contents
+        public IQueryable<Content> GetContent()
+        {
+            return db.Content;
+        }
+
+        // GET: api/Contents/5
+        [ResponseType(typeof(Content))]
+        public async Task<IHttpActionResult> GetContent(string id)
+        {
+            Content content = await db.Content.FindAsync(id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(content);
+        }
+
+        [AllowAnonymous]
+        // PUT: api/Contents/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutContent(string id, Content content)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != content.GUID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(content).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Contents
+        [ResponseType(typeof(Content))]
+        public async Task<IHttpActionResult> PostContent(Content content)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Content.Add(content);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ContentExists(content.GUID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = content.GUID }, content);
+        }
+
+        // DELETE: api/Contents/5
+        [ResponseType(typeof(Content))]
+        public async Task<IHttpActionResult> DeleteContent(string id)
+        {
+            Content content = await db.Content.FindAsync(id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+
+            db.Content.Remove(content);
+            await db.SaveChangesAsync();
+
+            return Ok(content);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ContentExists(string id)
+        {
+            return db.Content.Count(e => e.GUID == id) > 0;
+        }
+    }
+}
--- Source/Persets/Persets.Backend/Controllers/UserController.cs
+++ Source/Persets/Persets.Backend/Controllers/UserController.cs
@@ -18,49 +18,48 @@ namespace Persets.Backend.Controllers
         public UserController(IMembershipService membershipService)
         {
             _membershipService = membershipService;
+        }   
+       
+        [Route("api/users")]
+        [HttpPost]
+        public HttpResponseMessage Register(HttpRequestMessage request, RegistrationViewModel user)
+        {
+            HttpResponseMessage response;
+
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, new { success = false });
+                }
+                else
+                {
+                    Users _user = _membershipService.CreateUser(user.CompleteName, user.UserName, user.Email, user.Password, user.BirthdayDate);
+
+                    if (_user != null)
+                    {
+                        response = request.CreateResponse(HttpStatusCode.OK, new { success = true });
+                    }
+                    else
+                    {
+                        response = request.CreateResponse(HttpStatusCode.OK, new { success = false });
+                    }
+                }
+
+                return response;
+            }
+            catch (DbUpdateException ex)
+            {
+                response = request.CreateResponse(HttpStatusCode.BadRequest, ex.InnerException.Message);
+            }
+            catch (Exception ex)
+            {
+                response = request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+            return response;
         }
 
-
-  
-       
-                [Route("api/users")]
-                [HttpPost]
-                public HttpResponseMessage Register(HttpRequestMessage request, RegistrationViewModel user)
-                {
-                    HttpResponseMessage response;
-
-                    try
-                    {
-                        if (!ModelState.IsValid)
-                        {
-                            response = request.CreateResponse(HttpStatusCode.BadRequest, new { success = false });
-                        }
-                        else
-                        {
-                            Users _user = _membershipService.CreateUser(user.CompleteName, user.UserName, user.Email, user.Password, user.BirthdayDate);
-
-                            if (_user != null)
-                            {
-                                response = request.CreateResponse(HttpStatusCode.OK, new { success = true });
-                            }
-                            else
-                            {
-                                response = request.CreateResponse(HttpStatusCode.OK, new { success = false });
-                            }
-                        }
-
-                        return response;
-                    }
-                    catch (DbUpdateException ex)
-                    {
-                        response = request.CreateResponse(HttpStatusCode.BadRequest, ex.InnerException.Message);
-                    }
-                    catch (Exception ex)
-                    {
-                        response = request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
-                    }
-
-                    return response;
-                }
+        
     }
 }
